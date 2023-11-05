e3af774584d19357e24ed61f1fd4b6cebf89a2a8
poolkit <pulkit.pk7@gmail.com>
2023-11-05 16:22:31 +0530

    added url scraping

--- src/url_scraper.py
+++ src/url_scraper.py
@@ -17,18 +17,22 @@ class FetchTextFromURL():
         return ' '.join(text_data)
 
     def complex_scraper(self, response):
-        soup = BeautifulSoup(response.content, 'html.parser')
-        text_elements = soup.find(class_="tv-content")
+        try:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            text_elements = soup.find(class_="tv-content")
 
-        props_id = text_elements.find('div', class_='js-news-story-container').get('data-props-id', None)
-        script_div = text_elements.find('script', type='application/prs.init-data+json')
-        json_data = json.loads(script_div.string)
+            props_id = text_elements.find('div', class_='js-news-story-container').get('data-props-id', None)
+            script_div = text_elements.find('script', type='application/prs.init-data+json')
+            json_data = json.loads(script_div.string)
 
-        ast_description = json_data[props_id]['story']['astDescription']
-        children = ast_description['children']
-        p_type_children = [child['children'][0] for child in children if child['type'] == 'p']
+            ast_description = json_data[props_id]['story']['astDescription']
+            children = ast_description['children']
+            p_type_children = [child['children'][0] for child in children if child['type'] == 'p']
 
-        return ' '.join(p_type_children)
+            return ' '.join(p_type_children)
+        
+        except Exception as e:
+            raise e
 
     def fetch_article(self):
         try:
@@ -55,6 +59,10 @@ if __name__=="__main__":
     # url = "https://in.tradingview.com/news/financemagnates:cb3e7b789094b:0-binance-morgan-stanley-kraken-and-more-executive-moves-of-the-week/"
     # url = "https://in.tradingview.com/news/DJN_SN20231104002098:0/"
     # url = "https://in.tradingview.com/news/DJN_SN20231104001905:0/"
-    url = "https://in.tradingview.com/news/te_news:392280:0-egypt-non-oil-private-sector-pmi-slips-to-5-month-low/"
+    # url = "https://in.tradingview.com/news/te_news:392280:0-egypt-non-oil-private-sector-pmi-slips-to-5-month-low/"
+
+
+
+    url = "https://in.tradingview.com/news/cointelegraph:759c80cff094b:0-what-is-metadata-in-blockchain-transactions/"
     processor = FetchTextFromURL(url)
     print(processor.fetch_article())
