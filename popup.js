c2844c0f4d13d6add44f11aae0370c6eb6edae2f
poolkit <pulkit.pk7@gmail.com>
2023-11-06 09:20:38 +0530

    connection established

--- popup.js
+++ popup.js
@@ -3,20 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const apiKeyInput = document.getElementById("api-key");
     const saveApiKeyButton = document.getElementById("save-api-key");
     const savedText = document.getElementById("saved-api");
-    const gettextView = document.getElementById("getText")
-    const homePageView = document.getElementById("homePage")
-    const changeApiButton = document.getElementById("change-api-key")
-    const summarizeTradingView = document.getElementById("summarize-tradingview")
-    const notTradingView = document.getElementById("not-tradingview")
+    const gettextView = document.getElementById("getText");
+    const homePageView = document.getElementById("homePage");
+    const displaySummaryView = document.getElementById("displaySummary");
+    const changeApiButton = document.getElementById("change-api-key");
+    const summarizeTradingView = document.getElementById("summarize-tradingview");
+    const notTradingView = document.getElementById("not-tradingview");
+    const summary = document.getElementById("api-summary");
+    const opinion = document.getElementById("api-opinion");
 
     const tradingViewPrefix = "https://in.tradingview.com/news/"
     const undesiredPrefix = "category="
-    // const summarizeButton = document.getElementById("summarize");
-
-    homePageView.style.display = "none";
-    gettextView.style.display = "none";
-    summarizeTradingView.style.display = "none";
-    notTradingView.style.display = "none";
 
     chrome.storage.sync.get("apiKey", function (result) {
         const loadedKey = result.apiKey;
@@ -50,4 +47,80 @@ document.addEventListener("DOMContentLoaded", function () {
             notTradingView.style.display = "block";
         }
     });
+
+    summarizeTradingView.addEventListener("click", async () => {
+        // Show a loading indicator, if desired
+        summarizeTradingView.disabled = true;
+
+        try {
+            const currentUrl = await getCurrentUrl();
+            const responseData = await fetchDataFromAPI(currentUrl);
+            const resultText = responseData.Result;
+            const splitResult = resultText.split('\nCATEGORY:');
+
+            const responseSummary = splitResult[0].trim();
+            const responseOpinion = splitResult[1].trim();
+
+            summary.textContent = responseSummary;
+            opinion.textContent = responseOpinion;
+
+            gettextView.style.display = "none";
+            displaySummaryView.style.display = "block";
+            summary.style.display = "block";
+        } catch (error) {
+            console.error("Error:", error);
+        } finally {
+            summarizeTradingView.disabled = false;
+        }
+    });
+
+    function getCurrentUrl() {
+        return new Promise((resolve, reject) => {
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                if (tabs && tabs[0] && tabs[0].url) {
+                    resolve(tabs[0].url);
+                } else {
+                    reject("Unable to retrieve current URL");
+                }
+            });
+        });
+    }
+
+    async function fetchDataFromAPI(url) {
+        const apiKey = await getApiKeyFromSyncStorage();
+
+        try {
+            const response = await fetch("https://poolkit-tradewise.hf.space/summarise", {
+                method: "POST", // or "GET" or "PUT" as needed
+                body: JSON.stringify({ openai_api_key: apiKey, url: url }), // Send the URL to the API
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                  }
+            });
+
+            if (!response.ok) {
+                throw new Error(`API request failed with status: ${response.status}`);
+            }
+
+            const responseData = await response.json();
+            return responseData;
+        } catch (error) {
+            throw new Error("Failed to fetch data from the API: " + error.message);
+        }
+    }
+
+    function getApiKeyFromSyncStorage() {
+        return new Promise((resolve, reject) => {
+            chrome.storage.sync.get(["apiKey"], (result) => {
+                if (chrome.runtime.lastError) {
+                    reject(chrome.runtime.lastError);
+                } else {
+                    resolve(result.apiKey);
+                }
+            });
+        });
+    }
+
+
 });
